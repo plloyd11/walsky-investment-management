e96ee40d8a637a9d47b60521d9327ccf3ca5499f
plloyd11 <plloyd11@gmail.com>
2019-11-07 11:17:54 -0500

    fixed login issue

--- src/js/auth.js
+++ src/js/auth.js
@@ -9,40 +9,47 @@ new Vue({
 
     methods: {
         register() {
-            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-                .then(() => window.location = '/dashboard')
-                .catch(error => alert(error.message))
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(() => (window.location = '/dashboard'))
+                .catch(error => alert(error.message));
         },
 
         signOut() {
-            firebase.auth().signOut()
+            firebase.auth().signOut();
         },
 
         signIn() {
-            firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-                .then(() => window.location = '/dashboard')
-                .catch(error => alert(error.message))
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(() => (window.location = '/dashboard'))
+                .catch(error => alert(error.message));
         },
 
         resetPassword() {
-            firebase.auth().sendPasswordResetEmail(this.email)
-                .then(() => window.location = '/login')
-                .catch(error => alert(error.message))
+            firebase
+                .auth()
+                .sendPasswordResetEmail(this.email)
+                .then(() => (window.location = '/login'))
+                .catch(error => alert(error.message));
         },
 
         signInWithGoogle() {
-            const provider = new firebase.auth.GoogleAuthProvider()
-            firebase.auth().signInWithPopup(provider)
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase
+                .auth()
+                .signInWithPopup(provider)
                 .catch(error => alert(error.message))
-                .then(() => window.location = '/dashboard')
-                .then(data => console.log(data.user, data.credentials.accessToken))
+                .then(() => (window.location = '/dashboard'))
+                .then(data => console.log(data.user, data.credentials.accessToken));
         }
     },
 
     created() {
         firebase.auth().onAuthStateChanged(user => {
-            this.authUser = user
-        })
+            this.authUser = user;
+        });
     }
-
-})
+});
