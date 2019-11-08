4c9474ce4a73a4b727a0e2f4716d495326c70ef7
plloyd11 <plloyd11@gmail.com>
2019-11-08 13:53:38 -0500

    homepage dashboard fix

---
+++ src/js/newsletter-redirect.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#newsletter-redirect',
+
+    data: {
+        email: '',
+        password: '',
+        authUser: null
+    },
+    
+    created() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.authUser = user;
+        });
+    }
+});
